816fe3564980f0b6d323b6097ef0f216fb9fd6a0
Petar Ivanov <pe6oiv@gmail.com>
2013-08-26 20:59:11 +0300

    ExpandableListAdapter Implementation
    Signed-off-by: Petar Ivanov <pe6oiv@gmail.com>

--- ExpeririMundi_v1/src/com/debug/experirimundi/DashboardActivity.java
+++ ExpeririMundi_v1/src/com/debug/experirimundi/DashboardActivity.java
@@ -1,16 +1,32 @@
 package com.debug.experirimundi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
+import com.debug.experirimundi.library.ExpandableListAdapter;
 import com.debug.experirimundi.library.UserFunctions;
 
 public class DashboardActivity extends Activity {
     UserFunctions userFunctions;
     Button btnLogout;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +37,74 @@ public class DashboardActivity extends Activity {
         // Check login status in database
         userFunctions = new UserFunctions();
         if(userFunctions.isUserLoggedIn(getApplicationContext())){
-            setContentView(R.layout.dashboard);
+            setContentView(R.layout.dashboard_v2);
             btnLogout = (Button) findViewById(R.id.btnLogout);
+
+            // get the listview
+            expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+            // preparing list data
+            prepareListData();
+
+            listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+            // setting list adapter
+            expListView.setAdapter(listAdapter);
+
+            // Listview Group click listener
+            expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+                @Override
+                public boolean onGroupClick(ExpandableListView parent, View v,
+                        int groupPosition, long id) {
+                    // Toast.makeText(getApplicationContext(),
+                    // "Group Clicked " + listDataHeader.get(groupPosition),
+                    // Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+
+            // Listview Group expanded listener
+            expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+                @Override
+                public void onGroupExpand(int groupPosition) {
+                    Toast.makeText(getApplicationContext(),
+                            listDataHeader.get(groupPosition) + " Expanded",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // Listview Group collasped listener
+            expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+                @Override
+                public void onGroupCollapse(int groupPosition) {
+                    Toast.makeText(getApplicationContext(),
+                            listDataHeader.get(groupPosition) + " Collapsed",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+            // Listview on child click listener
+            expListView.setOnChildClickListener(new OnChildClickListener() {
+
+                @Override
+                public boolean onChildClick(ExpandableListView parent, View v,
+                        int groupPosition, int childPosition, long id) {
+                    // TODO Auto-generated method stub
+                    Toast.makeText(
+                            getApplicationContext(),
+                            listDataHeader.get(groupPosition)
+                                    + " : "
+                                    + listDataChild.get(
+                                            listDataHeader.get(groupPosition)).get(
+                                            childPosition), Toast.LENGTH_SHORT)
+                            .show();
+                    return false;
+                }
+            });
 
             btnLogout.setOnClickListener(new View.OnClickListener() {
 
@@ -46,9 +128,51 @@ public class DashboardActivity extends Activity {
             // Closing dashboard screen
             finish();
         }
+    }
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
+        // Adding child data
+        listDataHeader.add("Tours");
+        listDataHeader.add("Races");
+        listDataHeader.add("Tour Edit Menu");
+        listDataHeader.add("Race Edit Menu");
 
+        // Adding child data
+        List<String> tour = new ArrayList<String>();
+        tour.add("Choose Tour");
+        tour.add("The Godfather");
+        tour.add("The Godfather: Part II");
+        tour.add("Pulp Fiction");
+        tour.add("The Good, the Bad and the Ugly");
+        tour.add("The Dark Knight");
+        tour.add("12 Angry Men");
 
+        List<String> race = new ArrayList<String>();
+        race.add("The Conjuring");
+        race.add("Despicable Me 2");
+        race.add("Turbo");
+        race.add("Grown Ups 2");
+        race.add("Red 2");
+        race.add("The Wolverine");
 
+        List<String> tourEdit = new ArrayList<String>();
+        tourEdit.add("New Tour");
+        tourEdit.add("Edit Tour");
+        tourEdit.add("Delete Tour");
+
+        List<String> raceEdit = new ArrayList<String>();
+        raceEdit.add("New Race");
+        raceEdit.add("Edit Race");
+        raceEdit.add("Delete Race");
+
+        listDataChild.put(listDataHeader.get(0), tour); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), race);
+        listDataChild.put(listDataHeader.get(2), tourEdit);
+        listDataChild.put(listDataHeader.get(3), raceEdit);
     }
 }
